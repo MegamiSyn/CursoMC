4f981874ed6faf85933bdeb71982cf9d74215115
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-18 01:55:53 -0300

    Criando operacao de instanciacao

--- src/main/java/jp/syned/cursomc/CursoMcApplication.java
+++ src/main/java/jp/syned/cursomc/CursoMcApplication.java
@@ -1,13 +1,31 @@
  package jp.syned.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jp.syned.cursomc.domain.Categoria;
+import jp.syned.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoMcApplication {
+public class CursoMcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categproarepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categproarepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }

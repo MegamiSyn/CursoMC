756f479ce65f24240838b0dd266136866a5d54d8
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-18 19:23:06 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/jp/syned/cursomc/resources/exception/StandardError.java
@@ -0,0 +1,41 @@
+package jp.syned.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private int status;
+	private String msg;
+	private Long timeStamp;
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public StandardError(int i, String msg, Long timeStamp) {
+		super();
+		this.setStatus(i);
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+}

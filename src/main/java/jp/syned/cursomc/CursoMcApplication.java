7cc573efcf5620da70fe182a40bc9d700fdf80fb
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-18 18:54:37 -0300

    Produto e associacao muitos para muitos

--- src/main/java/jp/syned/cursomc/CursoMcApplication.java
+++ src/main/java/jp/syned/cursomc/CursoMcApplication.java
@@ -1,5 +1,7 @@
  package jp.syned.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import jp.syned.cursomc.domain.Categoria;
+import jp.syned.cursomc.domain.Produto;
 import jp.syned.cursomc.repositories.CategoriaRepository;
+import jp.syned.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categproarepository;
+	
+	@Autowired
+	private ProdutoRepository produtorepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -25,7 +32,19 @@ public class CursoMcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora",   800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categproarepository.saveAll(Arrays.asList(cat1,cat2));
+		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }

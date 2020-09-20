af7a20f3b29f574378b2e2099da27c0a63583610
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-20 01:17:19 -0300

    Validação sintática com Bean Validation

--- src/main/java/jp/syned/cursomc/dto/CategoriaDTO.java
+++ src/main/java/jp/syned/cursomc/dto/CategoriaDTO.java
@@ -1,12 +1,17 @@
 package jp.syned.cursomc.dto;
 
 import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
 import jp.syned.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5,max=80,message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/jp/syned/cursomc/services/CategoriaService.java
+++ src/main/java/jp/syned/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import jp.syned.cursomc.domain.Categoria;
+import jp.syned.cursomc.dto.CategoriaDTO;
 import jp.syned.cursomc.repositories.CategoriaRepository;
 import jp.syned.cursomc.services.exceptions.DataIntegrityException;
 import jp.syned.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy,String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy); 
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }

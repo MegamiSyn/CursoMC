d1d1453723d0ebebbefc242568c327c5e42f315d
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-26 01:46:21 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/jp/syned/cursomc/config/DevConfig.java
+++ src/main/java/jp/syned/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import jp.syned.cursomc.services.DBService;
+import jp.syned.cursomc.services.EmailService;
+import jp.syned.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
